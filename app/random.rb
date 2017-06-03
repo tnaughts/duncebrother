390ed9b106ec1523929edc50464ad2963b1677ea
tnaughts <tnaughts@gmail.com>
2017-06-03 12:18:17 -0500

    updates

--- app/random.rb
+++ app/random.rb
@@ -1,2 +1,25 @@
+# Services::Notifications::Sms.new({body: "Dunce Brother Automated Messaging"}, "+18159314369").send_message
+
 require 'services/notifications/sms'
-Services::Notifications::Sms.new({body: "Dunce Brother Automated Messaging"}, "+18159314369").send_message
+
+
+emails = [["tnaughts@gmail.com", "Tim", "+18159314369"],
+["anthony.m.lotesto@gmail.com", "Anthony", "+18159222518"],
+["mattwilliambee@gmail.com", "Matt", "+18154095642"],
+["jjnagle@gmail.com", "John", "+16189678309"],
+["terence.naughton@gmail.com", "Terry", "+13127719901"],
+["hardig1@yahoo.com", "Digz", "+16307231019"],
+["dklang22@gmail.com", "Daniel", "+17089459188"],
+["rhaaksma15@gmail.com", "Shmo", "+18152121858"],
+["charlesmack07@gmail.com", "Charlie", "+13092362193"],
+["corey.tallent9@gmail.com", "Corey", "+16306054016"],
+["dllibert1822@gmail.com", "Dave", "+16302179143"],
+["eric.mclean82@gmail.com", "Eric", "+18152603729"],
+["niemiec005@gmail.com", "Nick", "+16303012319"],
+["bee25141@gmail.com", "Tony", "+18156004269"],
+["grant@email", "Grant", "+16307401150"],
+["email", "Ron", "+18155450760"]]
+emails.each do |person|
+
+  Services::Notifications::Sms.new({body: "Dunce Brother Automated Messaging: '#{person[1]}', The date has been set for Dunce V. July 7-9th, Michigan City. Check duncebrother.com/v for other updates and email tinytony@duncebrother.com if plan to not attend"}, person[2]).send_message
+end
--- lib/services/notifications/sms.rb
+++ lib/services/notifications/sms.rb
@@ -2,17 +2,18 @@ require 'twilio-ruby'
 module Services
   module Notifications
     class Sms
-      attr_reader :payload, :phone
+      attr_reader :payload, :phone, :twilio_client
 
       def initialize(payload = {}, phone)
         @payload = payload
         @phone = phone
-        @twilio_client = Twilio::REST::Client.new
+        @twilio_client = Twilio::REST::Client.new "AC341b60ced7397fa99d3786c3b3c8e033", "fb737ee0fd817e98b1b5ad83d28dfb53"
       end
 
       def send_message
+
         twilio_client.account.messages.create(
-          from: ENV['TWILIO_NUMBER'],
+          from: "+17792054810",
           to: phone,
           body: payload[:body]
         )
